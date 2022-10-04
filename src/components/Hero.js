93d5d833f2920fa44d67ba54f594b7a94f130449
KrishayNair <krishay958@gmail.com>
2022-10-04 18:36:48 +0530

    images and Navbar added

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import bg1 from "../images/bg-1.jpg"
+import "./Hero.css"
+function Hero() {
+  return (
+    <div>
+        <section className='Main'>
+
+
+        </section>
+    </div>
+  )
+}
+
+export default Hero
