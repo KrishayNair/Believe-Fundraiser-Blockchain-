81a7c3e163a6ef7378acbbfcd424e4907eb047e0
KrishayNair <krishay958@gmail.com>
2022-10-04 14:43:26 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import './App.css';
 
 function App() {
